71830552a3ffb8b31a37fe398b396850e6248eb9
Le Chi Thu <thu2004@yahoo.se>
2011-05-01 16:38:46 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ViffNu::Application.config.secret_token = '48b9af5896db77ecd3af4f9680a71c6317154551de932348c69d705355add96f09484738a8bb0f0dc6ff669d30de0d62ab3f2858e9df5c03077270eecaf1ee74'
